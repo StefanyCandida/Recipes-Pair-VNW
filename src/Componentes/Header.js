8b4ebea4372ceadcf08c373426e32df898c1947d
DyanaFurriel <dyanafurriel.dsf@gmail.com>
2022-09-29 00:06:24 -0300

    Header finalizado

--- src/Componentes/Header.js
+++ src/Componentes/Header.js
@@ -1,29 +1,28 @@
 import React from 'react'
 import Rc from './Img/RC.svg'
+import './Header.css';
 
 export default function Header (){
 
-return(
-    <div>
+    return (
         <div>
-<figure>
-<img src={Rc}/>
-</figure>
+            <div className='header'>
+                <figure>
+                    <img src={Rc}/>
+                </figure>
+    
+                <nav className='top-menu'>
+                    <ul>
+                        <li>ABOUT</li>
+                        <li>RECIPES</li>
+                        <li>SUBSCRIBE</li>
+                    </ul>
+                </nav>
+            </div>
 
-<nav>
-<ul>
-    <li>ABOUT</li>
-    <li>RECIPES</li>
-    <li>SUBSCRIBE</li>
-</ul>
-</nav>
+            <div className='main-title'>
+                <h1>RECIPES</h1>
+            </div>
         </div>
-
-        <div>
-<h1>
-    RECIPES
-</h1>
-        </div>
-    </div>
-)
+    )
 }
